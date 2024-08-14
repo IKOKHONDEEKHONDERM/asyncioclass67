3c6113162e29db9760a4b8c458ed7aa05f0cee21
IKOKHONDEEKHONDERM <ikoboonmaleetrakun@gmail.com>
2024-08-14 10:03:50 +0700

    update

--- assignment08/flask-async/app.py
+++ assignment08/flask-async/app.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template
-import asyncio
+from flask import Flask , render_template
 import httpx
 import time
 import random
 from pypokemon.pokemon import Pokemon
-
+import asyncio
 
 app = Flask(__name__)
 
 async def get_pokemon(client, url):
-    pass
+    print(f"{time.ctime()} - get {url}")
+    resp = await client.get(url)
+    pokemon = resp.json()
+    return pokemon
 
 async def get_pokemons():
-    pass
+    rand_list = []
+    for i in range(5):
+        rand_list.append(random.randint(1,151))
+    #[random.randint(1, 151) for _ in range(5)]
+    pokemon_data = []
+
+    async with httpx.AsyncClient() as client:
+        tasks = [get_pokemon(client, f'https://pokeapi.co/api/v2/pokemon/{number}') for number in rand_list]
+        pokemons_json = await asyncio.gather(*tasks)
+
+    for pokemon_json in pokemons_json:
+        pokemon_object = Pokemon(pokemon_json)
+        pokemon_data.append(pokemon_object)
+        
+    return pokemon_data
 
 @app.route('/')
 async def index():
-    pass
+    start_time = time.perf_counter()
+    pokemons = await get_pokemons()
+    end_time = time.perf_counter()
+    print(f"{time.ctime()} - Get {len(pokemons)} pokemons. Time taken: {end_time-start_time} seconds")
+    return  render_template('index.html', pokemons=pokemons, end_time=end_time, start_time=start_time)
+
 if __name__ == '__main__':
     app.run(debug=True, port=50001)
--- assignment08/pokemon/pokemon03.py
+++ assignment08/pokemon/pokemon03.py
@@ -1,21 +1,37 @@
 from pypokemon.pokemon import Pokemon
-import asyncio
 import httpx
-import time
+import asyncio
 import random
+import time
 
 async def get_pokemon(client, url):
-    pass
+    print(f"time.client() = get {url}")
+    resp = await client.get(url)
+    pokemon = resp.json()
+
+    return Pokemon(pokemon)
 
 async def get_pokemons():
     async with httpx.AsyncClient() as client:
-        pass
+        tasks = []
+        rand_list = []
+        for i in range(151):
+            rand_list.append(random.randint(1, 151))
+
+        for number in rand_list:
+            url = f"https://pokeapi.co/api/v2/pokemon/{number}"
+            tasks.append(asyncio.create_task(get_pokemon(client, url)))
+
+        pokemons = await asyncio.gather(*tasks)
+
+    return pokemons
 
 async def index():
     start_time = time.perf_counter()
     pokemons = await get_pokemons()
     end_time = time.perf_counter()
-    print(f"{time.ctime()} - Asynchronous get {len(pokemons)} pokemons. Time taken: {end_time-start_time} seconds")
+    print(f"time.client() = Asynchronous get {len(pokemons)} pokemons. Time taken = {end_time - start_time} seconds")
+    return pokemons
 
-if __name__ == '__main__':
-   asyncio.run(index())
+if __name__ == "__main__":
+    asyncio.run(index())
