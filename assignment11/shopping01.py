f4c0a022c1e640b782078ce6507b755187f67240
IKOKHONDEEKHONDERM <ikoboonmaleetrakun@gmail.com>
2024-09-11 12:11:54 +0700

    update

--- assignment11/shopping01.py
+++ assignment11/shopping01.py
@@ -1,12 +1,7 @@
 import time
 import asyncio
 from asyncio import Queue
-from random import randrange
 
-# we first implement the Customer and Product classes, 
-# representing customers and products that need to be checked out. 
-# The Product class has a checkout_time attribute, 
-# which represents the time required for checking out the product.
 class Product:
     def __init__(self, product_name: str, checkout_time: float):
         self.product_name = product_name
